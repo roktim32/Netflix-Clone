09180ff3549bf356f7ee03222e55e01b93a16fd3
roktim32 <roktimsenapoty2@gmail.com>
2021-07-19 01:53:42 +0530

    homescreen floating button

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.grey[850],
+        child: const Icon(Icons.cast),
+        onPressed: ()=>print('CAST'),
+      ),
+    );
+  }
+}
