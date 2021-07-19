ad55ab9da1805100030efb12d9f9937a5af4f6c8
roktim32 <roktimsenapoty2@gmail.com>
2021-07-20 02:03:55 +0530

    preview

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -47,11 +47,42 @@ class _HomeScreenState extends State<HomeScreen> {
           scrollOffset: _scrollOffset,
         ),
       ),
-      body: CustomScrollView( 
+      body: CustomScrollView(
         controller: _scrollController,
         slivers: [
           SliverToBoxAdapter(
-              child: ContentHeader(featuredContent: sintelContent))
+            child: ContentHeader(featuredContent: sintelContent),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.only(top: 20.0),
+            sliver: SliverToBoxAdapter(child: 
+            Previews(
+              title:'Previews',
+              contentList: previews,
+
+            ),
+            ),
+
+          ),
+          SliverToBoxAdapter(
+            child: ContentList(
+              title:'My List',
+              contentList:myList,
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: ContentList(
+              title:'Netflix Originals',
+              contentList:originals,
+              isOriginals: true,
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: ContentList(
+              title:'Trending',
+              contentList:trending,
+            ),
+          ),
         ],
       ),
     );
---
+++ lib/widgets/content_list.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_netflix_responsive_ui/models/models.dart';
+
+class ContentList extends StatelessWidget {
+  final String title;
+  final List<Content> contentList;
+  final bool isOriginals;
+
+  const ContentList(
+      {Key key, this.title, this.contentList, this.isOriginals = false})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Text(
+            title,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
