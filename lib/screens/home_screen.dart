f2007b477de13d44f58ba5e602ab7313a3fed2fb
roktim32 <roktimsenapoty2@gmail.com>
2021-07-20 02:44:22 +0530

    bottom navigation bar added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -55,32 +55,33 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
           SliverPadding(
             padding: const EdgeInsets.only(top: 20.0),
-            sliver: SliverToBoxAdapter(child: 
-            Previews(
-              title:'Previews',
-              contentList: previews,
-
-            ),
-            ),
-
-          ),
-          SliverToBoxAdapter(
-            child: ContentList(
-              title:'My List',
-              contentList:myList,
+            sliver: SliverToBoxAdapter(
+              child: Previews(
+                title: 'Previews',
+                contentList: previews,
+              ),
             ),
           ),
           SliverToBoxAdapter(
             child: ContentList(
-              title:'Netflix Originals',
-              contentList:originals,
+              title: 'My List',
+              contentList: myList,
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: ContentList(
+              title: 'Netflix Originals',
+              contentList: originals,
               isOriginals: true,
             ),
           ),
-          SliverToBoxAdapter(
-            child: ContentList(
-              title:'Trending',
-              contentList:trending,
+          SliverPadding(
+            padding: const EdgeInsets.only(bottom: 20.0),
+            sliver: SliverToBoxAdapter(
+              child: ContentList(
+                title: 'Trending',
+                contentList: trending,
+              ),
             ),
           ),
         ],
