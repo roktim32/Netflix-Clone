7b9afa7a12c70f1fca164a7183f44400024e50ac
roktim32 <roktimsenapoty2@gmail.com>
2021-07-20 13:26:07 +0530

    reponsive

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Netflix UI',
       theme: ThemeData(
         primarySwatch: Colors.blue,
